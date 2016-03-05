74c612cfa622684bfb1b5d15e446c6871aa4a1d8
jultsvirbut <jul.tsvirbut@gmail.com>
2016-03-05 23:03:40 +0300

    add function pluralFormEnding

--- functions.php
+++ functions.php
@@ -34,7 +34,12 @@ function pluralForm($number, $after) {
 }
 
 
-    
+function pluralFormEnding($number, $afterEnds) {
+  $cases = array (2, 0, 1, 1, 1, 2);
+  echo $afterEnds[ ($number%100>4 && $number%100<20)? 2: $cases[min($number%10, 5)] ];
+}
+  
+
 function cut_zero($number) {
         
     $original_number = $number;
